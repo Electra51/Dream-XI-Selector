23c8acd07d87a70d8e7c5583d5c64600d453b864
safayet nur <safayetnurelectra@gmail.com>
2025-11-28 20:38:03 +0600

    update:Dream XI Selector project design improvement

--- select.js
+++ select.js
@@ -1,80 +1,42 @@
-
-
 const cartArray = [];
-
-
 
 function display(cartProduct) {
   console.log(cartProduct);
-  
-  const tablebody = document.getElementById('cart-product');
-  
+
+  const tablebody = document.getElementById("cart-product");
+
   const cartArrayLength = cartArray.length;
   if (cartArrayLength > 5) {
-    
-    alert('you can not added more than 5 player')
+    alert("you can not added more than 5 player");
     disable();
-    
-  }
-  else {
-    document.getElementById('total-added-player').innerText = cartArray.length;
+  } else {
+    document.getElementById("total-added-player").innerText = cartArray.length;
     tablebody.innerHTML = ``;
 
-    
-  for (let i = 0; i < cartProduct.length; i++){
-   
-    // console.log(cartArray[i].playerName);
-    const name = cartArray[i].playerName;
+    for (let i = 0; i < cartProduct.length; i++) {
+      const name = cartArray[i].playerName;
 
-    const tr = document.createElement("tr");
-    tr.innerHTML = `<th>${i+1}</th>
-    <td>${name}</td>`
-    tablebody.appendChild(tr);
-
-
+      const tr = document.createElement("tr");
+      tr.innerHTML = `<th>${i + 1}</th>
+    <td>${name}</td>`;
+      tablebody.appendChild(tr);
+    }
   }
-  }
-
-
 }
-
 
 function disable(element) {
-  if(element.disabled = true){
-    element.style.background = 'rgb(155, 173, 201)';
-    // alert(element.value);
+  if ((element.disabled = true)) {
+    element.style.background = "rgb(155, 173, 201)";
   }
 }
-
-
-
-
-
-
-  
-
 
 function addToCart(element) {
-  // if(element.disabled = true){
-  //   element.style.background = 'rgba(65, 128, 245, 0.527)';
-  //   // alert(element.value);
-  // }
- 
-  // element.disabled = true;
- 
-  
   const playerName = element.parentNode.children[0].innerText;
-  
+
   const playerobj = {
-    playerName: playerName
-  }
-  // document.getElementById("myBtn").disabled = true;
-  
+    playerName: playerName,
+  };
+
   cartArray.push(playerobj);
-  // console.log(cartArray);
-  // console.log(cartArray.length);
-  // document.getElementById('total-added-player').innerText = cartArray.length;
   display(cartArray);
 }
-
-
